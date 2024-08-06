e8573264120c5fb1f91007012b1766588ab72140
RieBi <42877671+RieBi@users.noreply.github.com>
2024-08-06 15:51:50 +0200

    Setup welcome screen

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,12 @@
 export default function Welcome() {
     return (
         <div className="
-        flex flex-col justify-center align-middle text-center mt-[30vh]
+        flex flex-col justify-center items-center text-center mt-[30vh]
         text-slate-700
         ">
-            <h1 className="font-bold text-3xl">Quizzical</h1>
+            <h1 className="font-bold text-3xl mb-2">Quizzical</h1>
             <p>A quiz game to test the sharpness of your mind</p>   
-            <button>
+            <button className="bg-indigo-500 text-white mt-5 px-24 py-4 rounded-xl">
                 <h2>Start quiz</h2>
             </button>
         </div>
